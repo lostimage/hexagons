1fa9b1a114d2aa07d98e7a77a328b1ade3fc1e9d
voltmod <bionikua@gmail.com>
2022-07-26 13:46:27 +0300

    Performance improvement

--- src/components/Home/HexagonItem/HexagonItem.js
+++ src/components/Home/HexagonItem/HexagonItem.js
@@ -13,7 +13,6 @@ const focusInput = ({element}) => {
     }, 0);
 }
 const HexagonItem = (props) => {
-    console.log('1')
     const [editable, setEditable] = useState(false)
     const [active, setActive] = useState(false);
     const itemRef = useRef(null)
